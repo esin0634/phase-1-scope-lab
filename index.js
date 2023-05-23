7aa35964742397fce4f9974b092436102f004dfa
Esin <nbusra.esin@gmail.com>
2023-05-23 16:58:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = "esin"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(customerName)
+
+}
+
+function setBestCustomer(){
+    var bestCustomer = "not bob"
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "esin"
+    
+}
+overwriteBestCustomer()
+
+const leastFavoriteCustomer = "ata"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "logan"
+    console.log(leastFavoriteCustomer)
+}
+
